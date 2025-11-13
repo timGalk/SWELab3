f39a2eacd2446ec7c9ba07057547a5ab3919beee
timGalk <tivron@outlook.com>
2025-11-13 11:53:32 +0100

    Add HRCalculator class and related test; update .gitignore and pom.xml

---
+++ src/test/java/HRCalculatorTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class HRCalculatorTest {
+    @Test
+    public void testValidMaxHR(){
+        var result = HRCalculator.calculateMaxHR(25);
+        assertEquals(195, result);
+    }
+}
